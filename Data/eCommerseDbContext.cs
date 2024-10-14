62b4a4bbe5c8c168afea6bedc2728aff602b1173
Jobirmizo <hishimovanvar@gmail.com>
2024-10-14 15:35:26 +0500

    mvc e-commerce not finished

---
+++ Data/eCommerseDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ecommerce.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ecommerce.Data;
+
+public class eCommerseDbContext : DbContext
+{
+    public eCommerseDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<BlogPost> BlogPosts { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    
+}
